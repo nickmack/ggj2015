7f2cb6cf0d4f78538422c2a169bfadb344c4c399
Nicholas Alexander <nickmaclewy222@gmail.com>
2015-01-23 23:11:52 -0300

    Fist commit

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+    private const string typeName = "GGJ2015";
+    private const string gameName = "mansion";
+
+    private HostData[] hostList;
+
+    private const int PORT = 22222;
+
+
+    private void StartServer()
+    {
+        Network.InitializeServer(4, PORT, !Network.HavePublicAddress());
+        MasterServer.RegisterHost(typeName, gameName);
+    }
+
+    void OnServerInitialized()
+    {
+        Debug.Log("Server Initializied");
+    }
+
+    private void JoinServer(HostData hostData)
+    {
+        Network.Connect(hostData);
+    }
+
+    void OnConnectedToServer()
+    {
+        Debug.Log("Server Joined");
+    }
+
+
+    private void RefreshHostList()
+    {
+        MasterServer.RequestHostList(typeName);
+    }
+
+    void OnMasterServerEvent(MasterServerEvent msEvent)
+    {
+        if (msEvent == MasterServerEvent.HostListReceived)
+            hostList = MasterServer.PollHostList();
+    }
+
+    void OnGUI()
+    {
+        if (!Network.isClient && !Network.isServer)
+        {
+            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+                StartServer();
+
+            if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+                RefreshHostList();
+
+            if (hostList != null)
+            {
+                for (int i = 0; i < hostList.Length; i++)
+                {
+                    if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+                        JoinServer(hostList[i]);
+                }
+            }
+        }
+    }
+
+}
